15d711120dae19a61c7a4722b8b3ce26e73a9046
Tyler Ruggles <truggles@wisc.edu>
2018-09-02 23:20:55 +0200

    L1CaloJets Phase-2 data format

--- DataFormats/Phase2L1CaloTrig/src/classes.h
+++ DataFormats/Phase2L1CaloTrig/src/classes.h
@@ -10,6 +10,7 @@
 /*********************/
 
 #include "DataFormats/Phase2L1CaloTrig/interface/L1EGCrystalCluster.h"
+#include "DataFormats/Phase2L1CaloTrig/interface/L1CaloJet.h"
 
 
 namespace {
@@ -20,6 +21,11 @@ namespace {
     std::vector<l1slhc::L1EGCrystalCluster>         l1egcrystalclustervec;
     l1slhc::L1EGCrystalClusterCollection            l1egcrystalclustercoll;
     edm::Wrapper<l1slhc::L1EGCrystalClusterCollection>   wl1egcrystalclustercoll;
+
+    l1slhc::L1CaloJet                               l1calojet;
+    std::vector<l1slhc::L1CaloJet>                  l1calojetvec;
+    l1slhc::L1CaloJetsCollection                    l1calojetcoll;
+    edm::Wrapper<l1slhc::L1CaloJetsCollection>      wl1calojetcoll;
 
   }
 }
